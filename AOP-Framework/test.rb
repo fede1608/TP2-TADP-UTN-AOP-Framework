b8694f6339c537681302a98b5898c9d1ad838a2d
overflowDT <fedecasares1993@yahoo.com.ar>
2013-10-04 22:33:59 -0300

    Agregado de definir clases por array, o por jeraquia (no incluye Object y BasicObject)

--- AOP-Framework/test.rb
+++ AOP-Framework/test.rb
@@ -21,31 +21,44 @@ class Bar
 
 end
 
+class Hola
+
+end
+
+class Chau < Hola
+end
+
 
 module AOPFramework
 
   def self.joint_point (args)
       clases=[]
     args.each do |decl,val|
-      p Object.subclasses
-       p clases << Object.subclasses.select{|klass| klass.to_s =~ (val)} if decl.to_s.start_with?('clase')
+
+       p clases << Object.subclasses.select{|klass| klass.to_s =~ (val)} if decl.to_s.start_with?('clase') && val.is_a?(Regexp)
+
+       p clases << val if decl.to_s.start_with?('clase') && val.is_a?(Array)
+
+       p clases << val.ancestors.select{|ances| ances.is_a?(Class) && ! Object.ancestors.include?(ances)} if decl.to_s.start_with?('hierarchy')
     end
-    clases.flatten!.uniq!
-      p "clases"
-    p clases
+    clases.flatten.uniq
   end
 
 end
 
 class Aspect
-  attr_reader :point_cuts
+  attr_accessor :point_cuts
   def initialize
     @point_cuts=[]
   end
 
 end
 
-AOPFramework::joint_point(clase:/Foo/, metodo:/[aeiou]/,clase2:/[123]/)
+p Object.subclasses
+aspecto= Aspect.new
+aspecto.point_cuts=(AOPFramework::joint_point(clase:/Foo/, metodo:/[aeiou]/,clase2:/Bar/,clase3:/Bar/,clase4:[Aspect,Foo],hierarchy:Chau))
+
+p aspecto.point_cuts
 
 
 
