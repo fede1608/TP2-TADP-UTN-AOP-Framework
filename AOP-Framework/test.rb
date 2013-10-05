c453b533720ea893e8c6a8b6d3bde555d2abd666
overflowDT <fedecasares1993@yahoo.com.ar>
2013-10-04 21:01:28 -0300

    Primeros JP

--- AOP-Framework/test.rb
+++ AOP-Framework/test.rb
@@ -1,14 +1,49 @@
+
 class Object
+  @@subclasses = []
+
   def self.inherited(subclass)
-    puts "New subclass: #{subclass}"
+    @@subclasses << subclass
+  end
+  def self.subclasses
+    @@subclasses
   end
 end
 
 class Foo
+
+end
+class Foo2
+
 end
 
-class Bar < Foo
+class Bar
+
 end
 
-class Baz < Bar
+
+module AOPFramework
+
+  def self.joint_point (args)
+
+    args.each do |decl,val|
+      p Object.subclasses
+       p Object.subclasses.select{|klass| klass.to_s =~ (val)} if decl.to_s.start_with?("clase")
+    end
+  end
+
 end
+
+
+AOPFramework::joint_point(clase:/Foo/, metodo:/[aeiou]/,clase2:/[123]/)
+
+
+
+
+
+
+
+
+#@@subclasses.each do |klass|
+# p klass.public_methods(false)
+#end
