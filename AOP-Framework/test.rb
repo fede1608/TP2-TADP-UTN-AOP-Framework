75f36e99571f850fd60eb08c02a2c87066c509a2
overflowDT <fedecasares1993@yahoo.com.ar>
2013-10-04 23:44:59 -0300

    Formateo de codigo

--- AOP-Framework/test.rb
+++ AOP-Framework/test.rb
@@ -65,39 +65,28 @@ module AOPFramework
       metodos=[]
       metodos_tot=[]
       args.each do |decl,val|
-
          p clases << Object.subclasses.select{|klass| klass.to_s =~ (val)} if decl.to_s.start_with?('clase') && val.is_a?(Regexp)
-
          p clases << val if decl.to_s.start_with?('clase') && val.is_a?(Array)
-
          p clases << val.ancestors.select{|ances| ances.is_a?(Class) && ! Object.ancestors.include?(ances)} if decl.to_s.start_with?('hierarchy')
-
       end
-
       clases = clases.flatten.uniq
-
       if clases==[]
         clases= Object.subclasses
       end
-
-
       clases.each do |klass|
         metodos_tot << klass.public_methods(false)
       end
-
       p metodos_tot = metodos_tot.flatten.uniq
       args.each do |decl,val|
         p metodos << metodos_tot.select{|metodo| metodo.to_s =~ val } if decl.to_s.start_with?('metodo') && val.is_a?(Regexp)
         p metodos << val if decl.to_s.start_with?('metodo') && val.is_a?(Array)
       end
-
       if (args[:accessors])
         clases.each do |klass|
           metodos << klass.attr_readers
           metodos << klass.attr_writers
         end
       end
-
       p metodos = metodos.flatten.uniq
       Hash[:clases => clases, :metodos => metodos]
   end
@@ -114,12 +103,10 @@ end
 
 p Object.subclasses
 aspecto= Aspect.new
-aspecto.point_cuts=(AOPFramework::joint_point(clase:/Foo/, metodo:/inher/,clase2:/Bar/,clase3:/Bar/,clase4:[Aspect,Foo],hierarchy:Chau,accessors:true))
+aspecto.point_cuts=(AOPFramework::joint_point(clase1:/Foo/, metodo1:/inher/,clase2:/Bar/,clase3:/Bar/,clase4:[Aspect,Foo],hierarchy:Chau,accessors:true))
 
 p aspecto.point_cuts
 
-
-p Aspect.attr_writers
 
 
 
